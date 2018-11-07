7d47aa0c4910e750438af05e002da9ab895db5e0
Andres Dalolio <amda95@hotmail.com>
2018-11-06 23:32:18 -0600

    sortAscending
    Ordenar lista generada por getSymbolsNumber de mayor a menor segun la Frecuencia

--- p2.erl
+++ p2.erl
@@ -5,6 +5,8 @@
 -export([countSymbol/2]).
 -export([generateList/2]).
 
+-export([sortAscending/1]).
+
 %https://stackoverflow.com/questions/14447575/reading-file-whole-flat-text-file-to-an-array
 
 %Leer contenido de archivo a binario:
@@ -24,3 +26,16 @@ getSymbolsNumber([H|T])->Number = countSymbol(H,T) , [[1 + Number|H]] ++
 								  getSymbolsNumber(T--generateList(H,Number)).
 
 %Generar arbol a partir de lista de simbolos:
+
+
+%Ordenar lista generada por getSymbolsNumber de mayor a menor segun la Frecuencia
+sortAscending(L) when length(L) =< 1 -> L;
+sortAscending(L) -> SL = sortAscendingP(L), sortAscending(lists:sublist(SL,1,length(SL)-1)) ++ [lists:last(SL)].
+
+sortAscendingP([])  -> [];
+sortAscendingP([F]) -> [F];
+sortAscendingP([[F1|F2],[G1|G2]|T]) when F1 > G1 -> [[G1|G2]|sortAscendingP([[F1|F2]|T])];
+sortAscendingP([[F1|F2],[G1|G2]|T]) -> [[F1|F2]|sortAscendingP([[G1|G2]|T])].
+
+
+
