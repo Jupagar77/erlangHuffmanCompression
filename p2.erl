d623d0ac8deca3db934d111cb855d04b7eb1ff5d
Andres Dalolio <amda95@hotmail.com>
2018-11-08 21:01:15 -0600

    Modifique sort

--- p2.erl
+++ p2.erl
@@ -28,14 +28,9 @@ getSymbolsNumber([H|T])->Number = countSymbol(H,T) , [[1 + Number|H]] ++
 %Generar arbol a partir de lista de simbolos:
 
 
-%Ordenar lista generada por getSymbolsNumber de mayor a menor segun la Frecuencia
-sortAscending(L) when length(L) =< 1 -> L;
-sortAscending(L) -> SL = sortAscendingP(L), sortAscending(lists:sublist(SL,1,length(SL)-1)) ++ [lists:last(SL)].
-
-sortAscendingP([])  -> [];
-sortAscendingP([F]) -> [F];
-sortAscendingP([[F1|F2],[G1|G2]|T]) when F1 > G1 -> [[G1|G2]|sortAscendingP([[F1|F2]|T])];
-sortAscendingP([[F1|F2],[G1|G2]|T]) -> [[F1|F2]|sortAscendingP([[G1|G2]|T])].
+%Ordenar lista generada por getSymbolsNumber de mayor a menor segun la Frecuencia. Basado en Quicksort
+sortAscending([]) -> [];
+sortAscending([[Pivot|Char]|T]) -> sortAscending([[X|Y] || [X|Y] <- T, X < Pivot]) ++ [[Pivot|Char]] ++ sortAscending([[X|Y] || [X|Y] <- T, X >= Pivot]).
 
 
 
