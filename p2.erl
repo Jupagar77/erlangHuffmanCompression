db38eedf5797192be931060b1617668ae76f979f
Andres Dalolio <amda95@hotmail.com>
2018-11-09 00:43:21 -0600

    Algoritmo Arbol Huffman

--- p2.erl
+++ p2.erl
@@ -7,6 +7,11 @@
 
 -export([sortAscending/1]).
 
+-export([crearListaArboles/1]).
+-export([sortAscendingArboles/1]).
+
+-export([huffman/1]).
+
 %https://stackoverflow.com/questions/14447575/reading-file-whole-flat-text-file-to-an-array
 
 %Leer contenido de archivo a binario:
@@ -31,6 +36,26 @@ getSymbolsNumber([H|T])->Number = countSymbol(H,T) , [[1 + Number|H]] ++
 %Ordenar lista generada por getSymbolsNumber de mayor a menor segun la Frecuencia. Basado en Quicksort
 sortAscending([]) -> [];
 sortAscending([[Pivot|Char]|T]) -> sortAscending([[X|Y] || [X|Y] <- T, X < Pivot]) ++ [[Pivot|Char]] ++ sortAscending([[X|Y] || [X|Y] <- T, X >= Pivot]).
+
+
+crearListaArboles([]) -> [];
+crearListaArboles(L) -> crearListaArboles(L,[]).
+crearListaArboles([],Aux) -> Aux;
+crearListaArboles([[F|E]|T],Aux) -> Laux = Aux ++ [{[F|E],{},{}}], crearListaArboles(T,Laux).
+
+
+sortAscendingArboles([]) -> [];
+sortAscendingArboles([{[Pivot|Char],I,D}|T]) -> sortAscendingArboles([{[X|Y],I,D} || {[X|Y],I,D} <- T, X < Pivot]) ++ [{[Pivot|Char],I,D}] ++ sortAscendingArboles([{[X|Y],I,D} || {[X|Y],I,D} <- T, X >= Pivot]).
+
+huffman([]) -> [];
+huffman([Arb|[]]) -> Arb;
+huffman([{[F1|E1],I1,D1},{[F2|E2],I2,D2}|[]]) 
+	-> Suma = F1+F2, NuevaRaiz = [Suma|null], {NuevaRaiz,{[F1|E1],I1,D1},{[F2|E2],I2,D2}};
+huffman([{[F1|E1],I1,D1},{[F2|E2],I2,D2}|T])
+	-> Suma = F1+F2, NuevaRaiz = [Suma|null], Arb = {NuevaRaiz,{[F1|E1],I1,D1},{[F2|E2],I2,D2}},
+	ListaActualizada = [Arb] ++ T, huffman(sortAscendingArboles(ListaActualizada)).
+
+
 
 
 
